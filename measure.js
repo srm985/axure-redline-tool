5b5ef5553aa857268e51152c8c8a1a18135b4dbf
Sean McQuay <smcquay@corp.securustech.net>
2017-06-01 11:22:58 -0500

    Working Save

--- measure.js
+++ measure.js
@@ -9,8 +9,11 @@ var borderThickness = 1,
     dimensionMarkerWidth = 0,
     dimensionMarkerHeight = 0;
 
+var cssProperties = { 'properties': { 'width': '', 'height': '' }, 'styles': { 'background-color': '', 'opacity': '', 'border-width': '', 'border-style': '', 'border-color': '', 'border-radius': '' }, 'text': { 'font-family': '', 'font-size': '', 'font-weight': '', 'color': '', 'test': '' } };
+
 $(document).ready(function() {
     $('.redline-layer').hide(); //*****Start by hiding all redline elements.*****
+    $('#redline-panel').show();
 
     $('body').on('mouseenter', '*', function() {
         hoveredElement = $(this);
@@ -36,6 +39,7 @@ $(document).ready(function() {
         if (!isRedlineElement(selectedElement)) {
             clearRedline();
             highlightSelectElement();
+            updateRedlinePanel(selectedElement);
         }
     });
 });
@@ -135,8 +139,8 @@ function measureIntraElementDistance() {
     } else if (elemSelectMeas.offsetTop < elemMeas.offsetTop && elemSelectMeas.offsetTop + elemSelectMeas.height < elemMeas.offsetTop + elemMeas.height) {
         intraElemMeas.bottom = Math.abs((elemMeas.offsetTop + elemMeas.height) - (elemSelectMeas.offsetTop + elemSelectMeas.height));
     } else {
-        intraElemMeas.top = Math.abs(elemMeas.offsetTop - elemSelectMeas.offsetTop);
-        intraElemMeas.bottom = Math.abs((elemMeas.offsetTop + elemMeas.height) - (elemSelectMeas.offsetTop + elemSelectMeas.height));
+        intraElemMeas.top = elemSelectMeas.offsetTop - elemMeas.offsetTop;
+        intraElemMeas.bottom = (elemMeas.offsetTop + elemMeas.height) - (elemSelectMeas.offsetTop + elemSelectMeas.height);
     }
 
     if (elemSelectMeas.offsetLeft > elemMeas.offsetLeft + elemMeas.width) {
@@ -148,8 +152,8 @@ function measureIntraElementDistance() {
     } else if (elemSelectMeas.offsetLeft < elemMeas.offsetLeft && elemSelectMeas.offsetLeft + elemSelectMeas.width < elemMeas.offsetLeft + elemMeas.width) {
         intraElemMeas.right = Math.abs((elemMeas.offsetLeft + elemMeas.width) - (elemSelectMeas.offsetLeft + elemSelectMeas.width));
     } else {
-        intraElemMeas.left = Math.abs(elemMeas.offsetLeft - elemSelectMeas.offsetLeft);
-        intraElemMeas.right = Math.abs((elemMeas.offsetLeft + elemMeas.width) - (elemSelectMeas.offsetLeft + elemSelectMeas.width));
+        intraElemMeas.left = elemSelectMeas.offsetLeft - elemMeas.offsetLeft;
+        intraElemMeas.right = (elemMeas.offsetLeft + elemMeas.width) - (elemSelectMeas.offsetLeft + elemSelectMeas.width);
     }
 }
 
@@ -160,42 +164,88 @@ function drawIntraElementMarkers() {
     dimensionMarkerWidth = $('.dimension-layer').width();
     dimensionMarkerHeight = $('.dimension-layer').height();
 
-    if (intraElemMeas.top > 0) {
+    if (intraElemMeas.top != 0) {
         $('#t-measure').show();
-        $('#t-measure').height(intraElemMeas.top - (borderThickness * 2));
-        $('#t-measure').offset({ top: elemSelectMeas.offsetTop - intraElemMeas.top, left: elemSelectMeas.offsetLeft + (elemSelectMeas.width / 2) });
+        $('#t-measure').height(Math.abs(intraElemMeas.top) - borderThickness);
+        if (intraElemMeas.top > 0) {
+            $('#t-measure').offset({ top: elemSelectMeas.offsetTop - intraElemMeas.top, left: elemSelectMeas.offsetLeft + (elemSelectMeas.width / 2) });
+        } else {
+            $('#t-measure').offset({ top: elemSelectMeas.offsetTop, left: elemSelectMeas.offsetLeft + (elemSelectMeas.width / 2) });
+        }
         $('#t-dimension').show();
         $('#t-dimension > span').show();
-        $('#t-dimension > span').html(Math.round(intraElemMeas.top));
+        $('#t-dimension > span').html(Math.round(Math.abs(intraElemMeas.top)));
         $('#t-dimension').offset({ top: elemSelectMeas.offsetTop - (intraElemMeas.top / 2) - (dimensionMarkerHeight / 2), left: elemSelectMeas.offsetLeft + (elemSelectMeas.width / 2) + labelSpacing });
     }
-    if (intraElemMeas.right > 0) {
+    if (intraElemMeas.right != 0) {
         $('#r-measure').show();
-        $('#r-measure').width(intraElemMeas.right - (borderThickness * 2));
-        $('#r-measure').offset({ top: elemSelectMeas.offsetTop + (elemSelectMeas.height / 2), left: elemSelectMeas.offsetLeft + elemSelectMeas.width });
+        $('#r-measure').width(Math.abs(intraElemMeas.right) - borderThickness);
+        if (intraElemMeas.right > 0) {
+            $('#r-measure').offset({ top: elemSelectMeas.offsetTop + (elemSelectMeas.height / 2), left: elemSelectMeas.offsetLeft + elemSelectMeas.width });
+        } else {
+            $('#r-measure').offset({ top: elemSelectMeas.offsetTop + (elemSelectMeas.height / 2), left: elemSelectMeas.offsetLeft + elemSelectMeas.width + intraElemMeas.right });
+        }
+
         $('#r-dimension').show();
         $('#r-dimension > span').show();
-        $('#r-dimension > span').html(Math.round(intraElemMeas.right));
+        $('#r-dimension > span').html(Math.round(Math.abs(intraElemMeas.right)));
         $('#r-dimension').offset({ top: elemSelectMeas.offsetTop + (elemSelectMeas.height / 2) - dimensionMarkerHeight - labelSpacing, left: elemSelectMeas.offsetLeft + elemSelectMeas.width + (intraElemMeas.right / 2) - (dimensionMarkerWidth / 2) });
     }
-    if (intraElemMeas.bottom > 0) {
+    if (intraElemMeas.bottom != 0) {
         $('#b-measure').show();
-        $('#b-measure').height(intraElemMeas.bottom - (borderThickness * 2));
-        $('#b-measure').offset({ top: elemSelectMeas.offsetTop + elemSelectMeas.height, left: elemSelectMeas.offsetLeft + (elemSelectMeas.width / 2) });
+        $('#b-measure').height(Math.abs(intraElemMeas.bottom) - borderThickness);
+        if (intraElemMeas.bottom > 0) {
+            $('#b-measure').offset({ top: elemSelectMeas.offsetTop + elemSelectMeas.height, left: elemSelectMeas.offsetLeft + (elemSelectMeas.width / 2) });
+        } else {
+            $('#b-measure').offset({ top: elemSelectMeas.offsetTop + elemSelectMeas.height + intraElemMeas.bottom, left: elemSelectMeas.offsetLeft + (elemSelectMeas.width / 2) });
+        }
+
         $('#b-dimension').show();
         $('#b-dimension > span').show();
-        $('#b-dimension > span').html(Math.round(intraElemMeas.bottom));
+        $('#b-dimension > span').html(Math.round(Math.abs(intraElemMeas.bottom)));
         $('#b-dimension').offset({ top: elemSelectMeas.offsetTop + elemSelectMeas.height + (intraElemMeas.bottom / 2) - (dimensionMarkerHeight / 2), left: elemSelectMeas.offsetLeft + (elemSelectMeas.width / 2) + labelSpacing });
     }
-    if (intraElemMeas.left > 0) {
+    if (intraElemMeas.left != 0) {
         $('#l-measure').show();
-        $('#l-measure').width(intraElemMeas.left - (borderThickness * 2));
-        $('#l-measure').offset({ top: elemSelectMeas.offsetTop + (elemSelectMeas.height / 2), left: elemSelectMeas.offsetLeft - intraElemMeas.left });
+        $('#l-measure').width(Math.abs(intraElemMeas.left) - borderThickness);
+        if (intraElemMeas.left > 0) {
+            $('#l-measure').offset({ top: elemSelectMeas.offsetTop + (elemSelectMeas.height / 2), left: elemSelectMeas.offsetLeft - intraElemMeas.left });
+        } else {
+            $('#l-measure').offset({ top: elemSelectMeas.offsetTop + (elemSelectMeas.height / 2), left: elemSelectMeas.offsetLeft });
+        }
+
         $('#l-dimension').show();
         $('#l-dimension > span').show();
-        $('#l-dimension > span').html(Math.round(intraElemMeas.left));
+        $('#l-dimension > span').html(Math.round(Math.abs(intraElemMeas.left)));
         $('#l-dimension').offset({ top: elemSelectMeas.offsetTop + (elemSelectMeas.height / 2) - dimensionMarkerHeight - labelSpacing, left: elemSelectMeas.offsetLeft - (intraElemMeas.left / 2) - (dimensionMarkerWidth / 2) });
     }
+}
+
+//*************************************************************************************************
+//*                                Update our redline spec panel.                                 *
+//*************************************************************************************************
+function updateRedlinePanel(element) {
+    $('#redline-panel *').remove('*');
+    $('#redline-panel').addClass('redline-panel-exposed');
+    $.each(cssProperties, function(i, value) {
+        $.each(cssProperties[i], function(_i, _value) {
+            cssProperties[i][_i] = element.css(_i);
+        });
+    });
+    appendRedlinePanel();
+}
+
+//*************************************************************************************************
+//*                                Append each property section.                                  *
+//*************************************************************************************************
+function appendRedlinePanel() {
+    $.each(cssProperties, function(i, value) {
+        $('#redline-panel').append('<div class="redline-layer redline-panel-section"></div>');
+        $('.redline-panel-section:last').append('<b><p class="redline-layer">' + i.toUpperCase() + '</p></b>');
+        $.each(cssProperties[i], function(_i, _value) {
+            $('.redline-panel-section:last').append('<p class="redline-layer">' + _i + ': ' + _value + '</p>');
+        });
+    });
 }
 
 //*************************************************************************************************
